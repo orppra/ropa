784ee62e1a37659f627ef49ec50a8c50421bb001
Jeff Sieu <jeffsy00@gmail.com>
2017-12-17 00:12:23 +0800

    Add file opening

--- gui.py
+++ gui.py
@@ -1,13 +1,13 @@
 import sys
-from PyQt4 import QtGui, uic
+from PyQt4 import QtGui as qg, QtCore as qc, uic
 
 Ui_MainWindow, QtBaseClass = uic.loadUiType('scene.ui')
-app = QtGui.QApplication(sys.argv)
+app = qg.QApplication(sys.argv)
 
 
-class App(QtGui.QMainWindow, Ui_MainWindow):
+class App(qg.QMainWindow, Ui_MainWindow):
     def __init__(self):
-        QtGui.QMainWindow.__init__(self)
+        qg.QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
 
@@ -16,11 +16,29 @@ def quit():
     app.quit()
 
 
+def open_file_dialog():
+    dialog = qg.QFileDialog()
+    dialog.setFileMode(qg.QFileDialog.AnyFile)
+    filenames = qc.QStringList()
+    if dialog.exec_():
+        filenames = dialog.selectedFiles()
+        for filename in filenames:
+            open_file(filename)
+
+
+def open_file(filename):
+    result = ''
+    with open(filename, 'r') as file:
+        for line in file:
+            result += line
+    return result
+
+
 def main():
     app_name = 'VsymX'
 
     # TODO: bind this thing correctly
-    # exit = app.findChild(QtGui.QAction, 'actionExit')
+    # exit = app.findChild(qg.QAction, 'actionExit')
     # exit.clicked.connect(quit)
 
     w = App()
@@ -28,6 +46,7 @@ def main():
     w.move(300, 300)
     w.setWindowTitle(app_name)
     w.show()
+    open_file_dialog()
     sys.exit(app.exec_())
 
     # setProperty('', value)
