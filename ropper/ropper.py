603d1e5bf33d3d09744ae18982a997644baf0adb
Owen Leong <owenl131@gmail.com>
2017-12-16 21:02:01 +0800

    Update

--- ropper/ropper.py
+++ ropper/ropper.py
@@ -2,61 +2,76 @@ import ropper
 from ropper import RopperService
 
 search_by = {
-	'generalSearch': search,
-	'byInstruction': search_instruction,
-	#'byOpcode': search_opcode,
-	'findJmpReg': search_jmpreg,
-	'findPopPopRet': search_poppopret,
+    'generalSearch': search,
+    'byInstruction': search_instruction,
+    #'byOpcode': search_opcode,
+    'findJmpReg': search_jmpreg,
+    'findPopPopRet': search_poppopret,
 }
 
 # poppopret: pop ???; pop ???; ret
 # jmpreg: jmp ???
 
+
 def get_filename():
-	pass
+    pass
+
 
 def get_architecture():
-	pass
+    pass
+
 
 def get_searchcommand():
-	pass
+    pass
+
 
 def get_ropchain():
-	# returns a list of list of tuples
-	pass
+    # returns a list of list of tuples
+    pass
+
 
 def add_file(service):
-	filename = get_filename()
-	if filename == None:
-		return 'Error: no file found'
-	service.addFile(filename)
-	return 'Success'
+    filename = get_filename()
+    if filename is None:
+        return 'Error: no file found'
+    service.addFile(filename)
+    return 'Success'
+
 
 def get_instance():
-	options = {'color': False,
-		   'badbytes': '',
- 		   'all': False,
-		   'inst_count': 6,
-		   'type': 'all',
-		   'detailed': False}
-	rs = RopperService(options)
-	return rs
+    options = {'color': False,
+               'badbytes': '',
+               'all': False,
+               'inst_count': 6,
+               'type': 'all',
+               'detailed': False}
+    rs = RopperService(options)
+    return rs
+
+
+def search():
+    pass
+
+
+def search_instruction():
+    pass
+
 
 def search_jmpreg():
-	pass
+    pass
+
 
 def search_poppopret():
-	pass
+    pass
+
 
 def close_file(service):
-	service.removeFile(get_filename())
-
-def 
-
+    service.removeFile(get_filename())
 
 
 def test():
-	pass
+    pass
+
 
 if __name__ == '__main__':
-	test()
+    test()
