140e5d9e2612b5fd4ff323b3f41e2fda80136e2d
Jeff Sieu <jeffsy00@gmail.com>
2017-12-16 23:41:36 +0800

    Refactor file menu

--- gui.py
+++ gui.py
@@ -2,6 +2,7 @@ import sys
 from PyQt4 import QtCore, QtGui, uic
 
 Ui_MainWindow, QtBaseClass = uic.loadUiType('scene.ui')
+app = QtGui.QApplication(sys.argv)
 
 class App(QtGui.QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -9,21 +10,25 @@ class App(QtGui.QMainWindow, Ui_MainWindow):
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
 
+def quit():
+    app.quit()
+
 def main():
-    app = QtGui.QApplication(sys.argv)
     app_name = 'VsymX'
 
-    app.findChild('')
+    # TODO: bind this thing correctly
+    # exit = app.findChild(QtGui.QAction, 'actionExit')
+    # exit.clicked.connect(quit)
 
     w = App()
     w.resize(1080, 720)
-    w.move(300, 300)te
+    w.move(300, 300)
     w.setWindowTitle(app_name)
     w.show()
     sys.exit(app.exec_())
 
     #setProperty('', value)
-    #findChild(name)
+    #findChild(child_type, child_name)
     #searchBar, searchButton
     
 if __name__=='__main__':
