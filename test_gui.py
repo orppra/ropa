5dfd57a72a513030feb08a9b2452182bc5ea37dd
Jeff Sieu <jeffsy00@gmail.com>
2017-12-16 21:56:43 +0800

    Add basic UI

---
+++ test_gui.py
@@ -0,0 +1,27 @@
+import sys
+from PyQt4 import QtCore, QtGui, uic
+
+class Scene(QtGui.QWidget):
+	def __init__(self):
+    	uic.loadUi('MyWidget.ui', self)
+    sys.exit(app.exec_())
+
+def main():
+    
+    app = QtGui.QApplication(sys.argv)
+
+    app_name = 'VsymX'
+
+    w = Scene()
+    w.resize(500, 500)
+    w.move(300, 300)
+    w.setWindowTitle(app_name)
+    w.show()
+
+    #setProperty('', value)
+    #findChild(name)
+    #searchBar, searchButton
+    
+
+if __name__=='__main__':
+	main()
