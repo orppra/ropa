ff4a8003ef1385b1a72579213ad2fcd88e910860
daniellim2000 <daniellimweesoong2000@gmail.com>
2017-12-17 03:19:19 +0800

    Fix parsing

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -1,10 +1,21 @@
-from backend.Backend import Backend
+import sys
+from os import path
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+from backend.Backend import Backend  # noqa
 
 backend = Backend(None)
 backend.set_filename('/tmp/a.out')
 backend.set_arch('x86')
 backend.activate()
+print('\n--- instruction: mov eax ---\n')
 res = backend.process_query('instruction', 'mov eax')
+print(res)
+
+# res = backend.process_query('jmp-reg', '')
+# print(res)
+
+print('\n--- pop pop ret ---\n')
+res = backend.process_query('pop-pop-ret', '')
 print(res)
 
 test_chain = []
--- tests/test_binary_struct.py
+++ tests/test_binary_struct.py
@@ -1,7 +1,2 @@
 p = ""
-p += struct.pack("<I", 0x811)  # mov eax, 0; pop rbp; ret; 
-p += struct.pack("<I", 0x850)  # mov eax, dword ptr [rip + 0x2007a2]; mov rdi, rax; call 0x6b0; nop; leave; ret; 
-p += struct.pack("<I", 0x789)  # mov eax, dword ptr [rip + 0x200861]; test rax, rax; je 0x7a0; pop rbp; jmp rax; 
-p += struct.pack("<I", 0x738)  # mov eax, dword ptr [rip + 0x20089a]; test rax, rax; je 0x750; pop rbp; jmp rax; 
-p += struct.pack("<I", 0x68d)  # mov eax, dword ptr [rip + 0x200955]; test rax, rax; je 0x69a; call rax; 
-p += struct.pack("<I", 0x68d)  # mov eax, dword ptr [rip + 0x200955]; test rax, rax; je 0x69a; call rax; add rsp, 8; ret; 
+p += struct.pack("<I", 0x8e0)  # p; o; p;  ; p; o; p;  ; r; e; t; ;; 
