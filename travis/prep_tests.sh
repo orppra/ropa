03e98096eec1ae3a9f07131af64b58860a04819c
daniellim2000 <daniellimweesoong2000@gmail.com>
2017-12-19 17:13:34 +0800

    Add sudo when installing dependencies in Travis

--- travis/prep_tests.sh
+++ travis/prep_tests.sh
@@ -10,11 +10,11 @@ fi
 test="$1"
 
 if [ "$test" = "static" ]; then
-    apt install -y python3-pip
-    python3 -m pip install -r requirements-devel.txt
+    sudo apt install -y python3-pip
+    sudo python3 -m pip install -r requirements-devel.txt
 elif [ "$test" = "ropa/tests/unit" ]; then
-    apt install -y python3-pip
-    python3 -m pip install -r requirements-devel.txt -r requirements.txt
+    sudo apt install -y python3-pip
+    sudo python3 -m pip install -r requirements-devel.txt -r requirements.txt
 else
     echo "Unknown test suite: $test"
     exit 1
