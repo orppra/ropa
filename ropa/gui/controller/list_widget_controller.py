c2a45d934f9b7d25d4a7f2f065366f469c73a2da
daniellimws <weesoong.lim@gmail.com>
2018-01-28 00:23:29 +0800

    Remove all old code comments

--- ropa/gui/controller/list_widget_controller.py
+++ ropa/gui/controller/list_widget_controller.py
@@ -15,17 +15,10 @@ class ListWidgetController:
         self.widget.clear()
         font = qg.QFont()
         font.setFamily(_fromUtf8('Courier new'))
-        # model = qg.QStandardItemModel(gadgets_list)
+
         for gadget in gadgets:
             cell = gadget['address'] + '\n'
-            # cell += '-' * 2 * len(str(gadget['address'])) + '\n'
             cell += '\n'.join(gadget['instructions']) + '\n'
-            # item = qg.QStandardItem(cell)
             item = qg.QListWidgetItem(qc.QString(cell), self.widget)
             item.setFont(font)
-            # item.setStatusTip(qc.QString(gadget['info']))
-            # item.setDragDropMode('InternalMove')
-            # model.appendRow(item)
             self.widget.insertItem(self.widget.count(), item)
-
-        # gadgets_list.setModel(model)
