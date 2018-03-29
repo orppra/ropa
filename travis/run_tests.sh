8bdb10b592d82f46095dc69836411b725722fd9e
daniellimws <weesoong.lim@gmail.com>
2018-03-29 22:44:58 +0800

    travis: Fix failing CI
    requirements-devel: fix pytest-qt version
    
    travis: Use pip2 instead of pip3
    
    travis: Use pip2 instead of pip3
    
    travis: Change pip3 to pip2 and install cmake

--- travis/run_tests.sh
+++ travis/run_tests.sh
@@ -10,12 +10,12 @@ fi
 test="$1"
 
 if [ "$test" = "static" ]; then
-    sudo apt install -y python3-pip
-    sudo python3 -m pip install -r requirements-devel.txt
+    sudo apt install -y python-pip
+    sudo python -m pip install -r requirements-devel.txt
 elif [ "$test" = "unit" ]; then
     travis/build_dependencies.sh
-    sudo apt install -y python3-pip
-    sudo python3 -m pip install -r requirements-devel.txt -r requirements.txt
+    sudo apt install -y python-pip cmake
+    sudo python -m pip install -r requirements-devel.txt -r requirements.txt
 else
     echo "Unknown test suite: $test"
     exit 1
