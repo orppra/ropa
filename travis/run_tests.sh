301053ab082b867b0f032d67d5069f50ebc97409
daniellimws <weesoong.lim@gmail.com>
2018-03-29 23:26:02 +0800

    travis: Change pip3 to pip2 and install cmake

--- travis/run_tests.sh
+++ travis/run_tests.sh
@@ -10,11 +10,11 @@ fi
 test="$1"
 
 if [ "$test" = "static" ]; then
-    sudo apt install -y python3-pip
+    sudo apt install -y python-pip
     sudo python -m pip install -r requirements-devel.txt
 elif [ "$test" = "unit" ]; then
     travis/build_dependencies.sh
-    sudo apt install -y python-pip
+    sudo apt install -y python-pip cmake
     sudo python -m pip install -r requirements-devel.txt -r requirements.txt
 else
     echo "Unknown test suite: $test"
