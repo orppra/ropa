c7cb50f455c37b219d31f4f6bb8bc79ca3908a4f
daniellimws <weesoong.lim@gmail.com>
2018-03-29 22:50:23 +0800

    travis: Use pip2 instead of pip3

--- travis/run_tests.sh
+++ travis/run_tests.sh
@@ -14,7 +14,7 @@ if [ "$test" = "static" ]; then
     sudo python3 -m pip install -r requirements-devel.txt
 elif [ "$test" = "unit" ]; then
     travis/build_dependencies.sh
-    sudo apt install -y python3-pip
+    sudo apt install -y python-pip
     sudo python3 -m pip install -r requirements-devel.txt -r requirements.txt
 else
     echo "Unknown test suite: $test"
