b3ec3dc9cffa65e25543d617ede945ca7f7be9bd
Daniel Lim Wee Soong <weesoong.lim@gmail.com>
2018-03-30 02:23:43 +0800

    Update run_tests.sh

--- travis/run_tests.sh
+++ travis/run_tests.sh
@@ -10,11 +10,11 @@ fi
 test="$1"
 
 if [ "$test" = "static" ]; then
-    sudo apt install -y python-pip3
+    sudo apt install -y python3-pip
     sudo python3 -m pip install -r requirements-devel.txt
 elif [ "$test" = "unit" ]; then
     travis/build_dependencies.sh
-    sudo apt install -y python-pip3 cmake
+    sudo apt install -y python3-pip cmake
     sudo python3 -m pip install -r requirements-devel.txt -r requirements.txt
 else
     echo "Unknown test suite: $test"
