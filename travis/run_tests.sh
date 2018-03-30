376d92ad01b54b9d2de64ebb7b63a65baa4dc0cd
daniellimws <weesoong.lim@gmail.com>
2018-03-30 11:15:52 +0800

    travis: Add upgrade flag to pip

--- travis/run_tests.sh
+++ travis/run_tests.sh
@@ -11,11 +11,11 @@ test="$1"
 
 if [ "$test" = "static" ]; then
     sudo apt install -y python3-pip
-    sudo python3 -m pip install -r requirements-devel.txt
+    sudo python3 -m pip install --upgrade -r requirements-devel.txt
 elif [ "$test" = "unit" ]; then
     travis/build_dependencies.sh
     sudo apt install -y python3-pip cmake
-    sudo python3 -m pip install -r requirements-devel.txt -r requirements.txt
+    sudo python3 -m pip install --upgrade -r requirements-devel.txt -r requirements.txt
 else
     echo "Unknown test suite: $test"
     exit 1
